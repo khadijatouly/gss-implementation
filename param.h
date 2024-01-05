9bdb22b471e180919a8ad924aa47ddb7eefa8383
ouzdeville <ouzdeville@gmail.com>
2024-01-05 12:44:36 +0000

    sdsq

--- param.h
+++ param.h
@@ -2,9 +2,9 @@
 #define PARAM_H
 
 #define BITS_PER_LONG (8 * sizeof(unsigned long))
-#define EXTENSION_DEGREE 5
-#define t 2
-#define code_length 28
+#define EXTENSION_DEGREE 11
+#define t 50
+#define code_length 1024
 #define mt (t*EXTENSION_DEGREE)
 #define code_dimension (code_length-mt)
 #define EXT_MU 1
