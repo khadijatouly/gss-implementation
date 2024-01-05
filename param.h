4bb354c42534c2485b58ffaeaef99bd04ca32a1c
ouzdeville <ouzdeville@gmail.com>
2024-01-05 14:56:56 +0000

    z

--- param.h
+++ param.h
@@ -4,7 +4,7 @@
 #define BITS_PER_LONG (8 * sizeof(unsigned long))
 #define EXTENSION_DEGREE 5
 #define t 2
-#define code_length 28
+#define code_length 18
 #define mt (t*EXTENSION_DEGREE)
 #define code_dimension (code_length-mt)
 #define EXT_MU 1
