4be762628dc390940d8ac5401320c757ec3daa35
ouzdeville <ouzdeville@gmail.com>
2024-01-05 14:56:23 +0000

    dsd

--- param.h
+++ param.h
@@ -3,7 +3,7 @@
 
 #define BITS_PER_LONG (8 * sizeof(unsigned long))
 #define EXTENSION_DEGREE 5
-#define t 4
+#define t 2
 #define code_length 28
 #define mt (t*EXTENSION_DEGREE)
 #define code_dimension (code_length-mt)
